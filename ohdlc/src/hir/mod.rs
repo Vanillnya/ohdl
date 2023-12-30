ccc9fd7e23d340a11d98935dbfd82efb168c16b4
DasLixou <einlixou@gmail.com>
2023-12-30 19:26:45 +0100

    TypeResolvingScopes

--- ohdlc/src/hir/mod.rs
+++ ohdlc/src/hir/mod.rs
@@ -1,18 +1,22 @@
-use std::fmt::Debug;
+use std::{collections::HashMap, fmt::Debug};
 
 use slab::Slab;
 
-use crate::symbol::Ident;
+use crate::symbol::{Ident, Symbol};
 
 pub mod stages;
 
 pub struct HIR<'hir> {
     pub types: Slab<Declaration<'hir>>,
+    pub tr_scopes: Slab<TypeResolvingScope>,
 }
 
 impl HIR<'_> {
     pub fn new() -> Self {
-        Self { types: Slab::new() }
+        Self {
+            types: Slab::new(),
+            tr_scopes: Slab::new(),
+        }
     }
 }
 
@@ -22,9 +26,38 @@ impl Debug for HIR<'_> {
     }
 }
 
+pub struct TypeResolvingScope {
+    pub parent: Option<usize>, // can we make a fork of slab with NonNull keys? :>
+    pub types: HashMap<Symbol, usize>,
+}
+
 #[derive(Debug)]
 pub enum Declaration<'hir> {
+    Record(Record),
     Enum(Enum<'hir>),
+}
+
+impl Declaration<'_> {
+    pub fn id(&self) -> usize {
+        match self {
+            Declaration::Record(r) => r.type_id,
+            Declaration::Enum(e) => e.type_id,
+        }
+    }
+
+    pub fn name(&self) -> Ident {
+        match self {
+            Declaration::Record(r) => r.name,
+            Declaration::Enum(e) => e.name,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct Record {
+    pub type_id: usize,
+    pub name: Ident,
+    // TODO: fields
 }
 
 #[derive(Debug)]
--- ohdlc/src/hir/stages/rough/mod.rs
+++ ohdlc/src/hir/stages/rough/mod.rs
@@ -1,8 +1,10 @@
+use std::collections::HashMap;
+
 use bumpalo::Bump;
 
 use crate::{
     ast::{self, Item},
-    hir::{Declaration, Enum, Variant, HIR},
+    hir::{Declaration, Enum, Record, TypeResolvingScope, Variant, HIR},
 };
 
 pub struct RoughLowering<'a, 'hir> {
@@ -12,15 +14,28 @@ pub struct RoughLowering<'a, 'hir> {
 
 impl<'hir> RoughLowering<'_, 'hir> {
     pub fn lower(&mut self, items: &[ast::Item]) {
+        let root_scope = self.hir.tr_scopes.insert(TypeResolvingScope {
+            parent: None,
+            types: HashMap::new(),
+        });
         for item in items {
-            self.lower_item(item);
+            self.lower_item(item, root_scope);
         }
     }
 
-    pub fn lower_item(&mut self, item: &Item) {
+    pub fn lower_item(&mut self, item: &Item, scope: usize) {
         match &item.base.0 {
+            ast::ItemBase::Record(r) => {
+                let id = self.hir.types.insert_with(|type_id| {
+                    Declaration::Record(Record {
+                        type_id,
+                        name: r.name,
+                    })
+                });
+                self.hir.tr_scopes[scope].types.insert(r.name.0, id); // TODO: check for collision
+            }
             ast::ItemBase::Enum(e) => {
-                self.hir.types.insert_with(|type_id| {
+                let id = self.hir.types.insert_with(|type_id| {
                     Declaration::Enum(Enum {
                         type_id,
                         name: e.name,
@@ -29,6 +44,7 @@ impl<'hir> RoughLowering<'_, 'hir> {
                         ),
                     })
                 });
+                self.hir.tr_scopes[scope].types.insert(e.name.0, id); // TODO: check for collision
             }
             _ => { /* TODO */ }
         }
