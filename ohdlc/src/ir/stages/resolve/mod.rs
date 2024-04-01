742b59aad837e71bd38ccb0aec37af7c3f12b171
DasLixou <einlixou@gmail.com>
2024-04-01 18:58:04 +0200

    Resolve up the scope tree

--- ohdlc/src/ir/stages/resolve/mod.rs
+++ ohdlc/src/ir/stages/resolve/mod.rs
@@ -1,6 +1,10 @@
 use bumpalo::Bump;
 
-use crate::ir::{resolving::Resolvable, IR};
+use crate::{
+    ir::{resolving::Resolvable, IR},
+    message::Message,
+    MESSAGES,
+};
 
 pub struct ResolveLowering<'ir, 'b> {
     pub arena: &'ir Bump,
@@ -12,17 +16,33 @@ impl<'ir> ResolveLowering<'ir, '_> {
         while let Some(id) = self.ir.name_resolution.queue.pop_front() {
             let import = &mut self.ir.name_resolution.imports[id];
             let segment = import.path.first().unwrap();
-            if let Some(&resolvable) = self.ir.resolving_scopes[import.src].entries.get(&segment) {
-                println!("{segment:?}");
-                match resolvable {
-                    Resolvable::Type(t) => todo!(),
-                    Resolvable::Module(m) => {
-                        let module = &self.ir.modules[m];
-                        import.src = module.scope;
-                        import.path = &import.path[1..];
+            println!("{segment:?}");
+
+            let resolvable = {
+                let mut scope = &self.ir.resolving_scopes[import.src];
+                'resolve: loop {
+                    match scope.entries.get(&segment) {
+                        Some(resolvable) => break 'resolve Some(resolvable),
+                        None => match scope.parent {
+                            Some(p) => scope = &self.ir.resolving_scopes[p],
+                            None => break 'resolve None,
+                        },
                     }
-                    Resolvable::Import(i) => todo!(),
                 }
+            };
+            let Some(resolvable) = resolvable else {
+                MESSAGES.report(Message::could_not_resolve(*segment));
+                continue;
+            };
+
+            match *resolvable {
+                Resolvable::Type(t) => todo!(),
+                Resolvable::Module(m) => {
+                    let module = &self.ir.modules[m];
+                    import.src = module.scope;
+                    import.path = &import.path[1..];
+                }
+                Resolvable::Import(i) => todo!(),
             }
         }
     }
